4588c426962dca34f2a6bffba76e139d05fd14a4
Thomas Meyer <tmeyer@yext.com>
2025-02-25 13:22:53 -0500

    Add Publisher Icons.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,38 @@
 import { ComponentConfig } from "@measured/puck";
+import YelpSvg from '../../static/svg/site-icon-15.svg';
+import FacebookSvg from '../../static/svg/site-icon-71.svg';
+import FirstPartySvg from '../../static/svg/site-icon-283.svg';
+
+enum Publishers {
+  "First Party" = "First Party",
+  "Yelp" = "Yelp",
+  "Facebook" = "Facebook"
+}
 
 interface CardProps {
   title: string,
   description: string,
-  icon?: string
+  publisher: Publishers
 }
 
-function CardInternal({ title, description, icon }: CardProps) {
+function CardInternal({ title, description, publisher }: CardProps) {
+  let iconSrc;
+  switch (publisher) {
+    case Publishers["First Party"]:
+      iconSrc = FirstPartySvg.src;
+      break;
+    case Publishers.Yelp:
+      iconSrc = YelpSvg.src;
+      break;
+    case Publishers.Facebook:
+      iconSrc = FacebookSvg.src;
+      break;
+  }
+
   return (
     <div className="Card">
       <div className="Card-inner">
+        <div className="Card-icon"><img src={iconSrc}/></div>
         <div className="Card-title">{title}</div>
         <div className="Card-description">{description}</div>
       </div>
@@ -24,6 +47,10 @@ export const Card: ComponentConfig<CardProps> = {
     },
     description: {
       type: "textarea"
+    },
+    publisher: {
+      type: "select",
+      options: Object.keys(Publishers).map(key => { return { label: key, value: key } })
     }
   },
   render: (props) => {
